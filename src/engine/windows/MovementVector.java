9ef578c7302c2d61acc80ada521d96a7ec84e955
snowflakes379 <snowflakes379@gm.com>
2023-04-18 23:42:55 +0700

    remove: files

---
+++ src/engine/windows/MovementVector.java
@@ -0,0 +1,11 @@
+package engine.windows;
+
+public class MovementVector {
+    public float x;
+    public float y;
+
+    public MovementVector(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
